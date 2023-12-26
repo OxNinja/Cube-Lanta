616708b464d20e31301e9853a5b028e322d18129
OxNinja <57400445+OxNinja@users.noreply.github.com>
2023-12-26 11:19:45 +0100

    Added team member set command (#2) Fixed empty team members list bug on toString()

--- cube-lanta/src/main/java/fr/xninja/cubelanta/CLTeam.java
+++ cube-lanta/src/main/java/fr/xninja/cubelanta/CLTeam.java
@@ -25,15 +25,33 @@ public class CLTeam {
         this.members = list;
     }
 
-    public void addMember(Player member) {
+    public Boolean addMember(Player member) {
+        Boolean result = false;
+
+        if(this.members.contains(member)) {
+            System.out.println("Player " + member.getName() + " is already in this team");
+            return result;
+        }
+
+        for(CLTeam team: CLGlobal.teams.values()) {
+            if(team.members.contains(member)) {
+                System.out.println("Player " + member.getName() + " is in another team: " + team.name);
+                team.removeMember(member);
+            }
+        }
+
+        result = true;
         this.members.add(member);
+        System.out.println("Player " + member.getName() + " has been added to team " + this.name);
+        return result;
     }
 
     public void removeMember(Player member) {
         int index = this.members.indexOf(member);
         if(index == -1) {
-            System.out.println("Member " + member.getName() + " not found in team " + this.name + " for remove.");
+            System.out.println("Player " + member.getName() + " not found in team " + this.name + " for remove.");
         } else {
+            System.out.println("Player " + member.getName() + " has been removed from team " + this.name);
             this.members.remove(index);
         }
     }
@@ -59,17 +77,18 @@ public class CLTeam {
 
     @Override
     public String toString() {
-        String resutl = "===== " + this.name + " =====\n";
+        String result = "===== " + this.name + " =====\n";
         String leaderName = "";
         if(this.getLeader() != null) {
             leaderName = this.getLeader().getName();
         }
-        resutl += "Leader: " + leaderName + "\n";
-        resutl += "Members: (" + String.valueOf(this.members.size()) + ")\n";
-        for(Player p: this.getMembers()) {
-            resutl += p.getName() + "\n";
+        result += "Leader: " + leaderName + "\n";
+        result += "Members: (" + String.valueOf(this.members.size()) + ")\n";
+        if(this.members.size() > 0) {
+            for(Player p: this.getMembers()) {
+                result += p.getName() + "\n";
+            }
         }
-
-        return resutl;
+        return result;
     }
 }
--- cube-lanta/src/main/java/fr/xninja/cubelanta/CubeLanta.java
+++ cube-lanta/src/main/java/fr/xninja/cubelanta/CubeLanta.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.xninja.cubelanta.CLCommands.CLCommandTeamCreate;
 import fr.xninja.cubelanta.CLCommands.CLCommandTeamSetLeader;
+import fr.xninja.cubelanta.CLCommands.CLCommandTeamSetMember;
 import fr.xninja.cubelanta.CLCommands.CLCommandTeamShow;
 import fr.xninja.cubelanta.CLCommands.CLCommandTeamsList;
 import fr.xninja.cubelanta.CLScoreboard.CLScoreboard;
@@ -24,6 +25,7 @@ public final class CubeLanta extends JavaPlugin {
 		this.getCommand("cl-teams-list").setExecutor(new CLCommandTeamsList());
 		this.getCommand("cl-team-show").setExecutor(new CLCommandTeamShow());
 		this.getCommand("cl-team-set-leader").setExecutor(new CLCommandTeamSetLeader());
+		this.getCommand("cl-team-set-member").setExecutor(new CLCommandTeamSetMember());
 
 		getLogger().info("CubeLanta is loaded!");
 	}
